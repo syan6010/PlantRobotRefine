90d03cbd404cadaba01247ae8b038a13e80efa67
syan6010 <monday6010@gmail.com>
2019-03-30 19:59:33 +0800

    firebase update

--- index.js
+++ index.js
@@ -35,18 +35,18 @@ bot.on('message', function (event) {
         if(snapshot.exists()) 
         {
                 step = await snapshot.val();
-                if (qAndAStep === 0 ) {
+                if (step === 0 ) {
                     await event.reply('你好!!歡迎來到plantRobot!!第一次設定需要輸入webduino裝置的ID才可以讓我順利上網歐！！');
-                } else if(qAndAStep === 1) {
+                } else if(step === 1) {
                     event.reply('可以告訴我你的植物種類嗎？');
                     updateData(lineId, "deviceId", event.message.text);
-                } else if(qAndAStep === 2) {
+                } else if(step === 2) {
                     event.reply('謝謝！我們又邁進了一步！！可以讓我知道要怎麼稱呼你嗎？');
                     updateData(lineId, "plantType", event.message.text);
-                } else if(qAndAStep === 3) {
+                } else if(step === 3) {
                     event.reply('謝謝接下來我們馬上就可以開始使用了！！輸入OK取得資訊!!!!!!!');
                     updateData(lineId, "name", event.message.text);
-                } else if(qAndAStep === 99) {
+                } else if(step === 99) {
                     event.reply('99')
                 }
 
