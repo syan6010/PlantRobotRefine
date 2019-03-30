72def1008ef7f6c7cae55bcf825b02180f052a65
syan6010 <monday6010@gmail.com>
2019-03-30 19:12:09 +0800

    firebase update

--- index.js
+++ index.js
@@ -9,16 +9,16 @@ const bot = linebot({
   channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN
 });
 
-var config = {
-    apiKey: "AIzaSyAH8TrJyIm1rc5Pf4Kv1QK1e196Btft_a8",
-    authDomain: "plantrobotrefine.firebaseapp.com",
-    databaseURL: "https://plantrobotrefine.firebaseio.com",
-    projectId: "plantrobotrefine",
-    storageBucket: "plantrobotrefine.appspot.com",
-    messagingSenderId: "686852886099"
-};
+// var config = {
+//     apiKey: "AIzaSyAH8TrJyIm1rc5Pf4Kv1QK1e196Btft_a8",
+//     authDomain: "plantrobotrefine.firebaseapp.com",
+//     databaseURL: "https://plantrobotrefine.firebaseio.com",
+//     projectId: "plantrobotrefine",
+//     storageBucket: "plantrobotrefine.appspot.com",
+//     messagingSenderId: "686852886099"
+// };
 
-firebase.initializeApp(config);
+// firebase.initializeApp(config);
 
 const app = express();
 
@@ -29,14 +29,7 @@ app.post('/linewebhook', linebotParser);
 
 
 bot.on('message', function (event) {
-    firebase.database().ref('users/' + lineId).set({
-        deviceId: 0,
-        plantType: 0,
-        name : 0,
-        dht : 0,
-        temperature : 0,
-        steps : 0
-    });
+    event.reply('hello')
 });
 
 
