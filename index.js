384d982fd4f1fe274686f7660ed81ad8803e8a92
syan6010 <monday6010@gmail.com>
2019-03-30 20:59:45 +0800

    firebase update

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ bot.on('message', function (event) {
         firebase.database().ref(`users/${lineId}/steps`).on('value', async function (snapshot) {
             if(snapshot.exists()) 
             {
-                event.reply('hello')
+                event.reply(snapshot.val())
             }
             else 
             {
