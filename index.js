096541a4d1936dca36177834748edb0a4896f27d
syan6010 <monday6010@gmail.com>
2019-04-01 17:21:54 +0800

    firebase test

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ bot.on('message', function (event) {
         firebase.database().ref(`users/${lineId}/steps`).on('value', function (snapshot) {
             if(snapshot.exists()) 
             {
-                event.reply('helo');
+                event.reply(snapshot.val());
             }
             else 
             {
