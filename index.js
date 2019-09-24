d48b865e761c36c646f0408cc4aa06fa9c0aa2ff
syan6010 <monday6010@gmail.com>
2019-09-24 09:04:25 +0800

    ui -complete2

--- index.js
+++ index.js
@@ -147,7 +147,7 @@ let initData = (lineId) => {
 plant_ref.once('value')
   .then(function(snapshot){
     snapshot.forEach(function (childSnapshot){
-      let dht = childSnapshot.child("dht").vao()
+      let dht = childSnapshot.child("dht").val()
       bot.push("U0b6e923254483d85b37802373341c02d", dht)
     })
   })
