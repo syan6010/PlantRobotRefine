edba008255bd9b6a444394f20bf72c81f09d779b
syan6010 <monday6010@gmail.com>
2019-09-24 09:10:47 +0800

    ui -complete2

--- index.js
+++ index.js
@@ -37,6 +37,8 @@ var large_b = ["橡皮樹", "發財樹"]; //大型室内植物
 
 let line_id_ref = firebase.database().ref(`user_device`)
 let plant_ref = firebase.database().ref(`plant_condition/123/2019-8-22`)
+let plant_ref1 = firebase.database().ref(`plant_condition/123/2019-8-22/-Lms3E2xpWh_JYTdgJMC/dht`)
+
 
 
 
@@ -144,12 +146,14 @@ let initData = (lineId) => {
 }
 
 
-plant_ref.once('value')
+plant_ref1.once('value')
   .then(function(snapshot){
-    snapshot.forEach(function (childSnapshot){
-      let dht = childSnapshot.child("dht").val()
-      bot.push("U0b6e923254483d85b37802373341c02d", dht)
-    })
+    // snapshot.forEach(function (childSnapshot){
+    //   let dht = childSnapshot.child("dht").val()
+    //   bot.push("U0b6e923254483d85b37802373341c02d", dht)
+    // })
+    let dht = snapshot.val()
+    bot.push("U0b6e923254483d85b37802373341c02d", dht)
   })
 
 
