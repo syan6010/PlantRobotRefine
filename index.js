b021cf272818eb8a5e23cf83dfff519705015914
syan6010 <monday6010@gmail.com>
2019-10-20 22:18:11 +0800

    時間測試

--- index.js
+++ index.js
@@ -318,9 +318,11 @@ const  scheduleCronstyle = ()=>{
                 bot.push(each_id, `狀況不太好欸！可以上我們的網站獲取植物冷知識，加油吧！今天我的平均溫度是${temperature_tot/2}, 濕度是${humidity_tot/2}, 總體溫濕度指標為${dht_tot/2 + 40}分，不符合標準`)
               }
             }
+
+            for_add_dht()
+            for_add_t_h()
             await push_fun()
-            await for_add_dht()
-            await for_add_t_h()
+
 
 
             // dht_tot = 0;
