20c100db30724f035b422cd32627c599238773cc
syan6010 <monday6010@gmail.com>
2019-10-20 19:57:02 +0800

    時間測試

--- index.js
+++ index.js
@@ -269,7 +269,7 @@ let initData = (lineId) => {
 
 
 const  scheduleCronstyle = ()=>{
-    schedule.scheduleJob('30 15 * * * *',()=>{
+    schedule.scheduleJob('30 58 * * * *',()=>{
       // let dht_tot = 0;
       // let temperature_tot = 0;
       // let humidity_tot = 0;
