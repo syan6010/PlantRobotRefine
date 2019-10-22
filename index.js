fdae23c529164ebd4e19a1f5a25cb0cec01a09cc
syan6010 <monday6010@gmail.com>
2019-10-22 16:28:51 +0800

    時間測試

--- index.js
+++ index.js
@@ -295,8 +295,8 @@ const  scheduleCronstyle = ()=>{
             let temperature_tot = 0;
             let humidity_tot = 0;
             let each_id = childSnapshot.key
-            let new_plant_ref = firebase.database().ref(`/plant_condition/${each_id}/${today.getFullYear()}-${today.getMonth()+1}-${today.getDate()-1}`)
-            let new_evo_ref = firebase.database().ref(`/environment_condition/${each_id}/${today.getFullYear()}-${today.getMonth()+1}-${today.getDate()-1}`)
+            let new_plant_ref = firebase.database().ref(`/plant_condition/${each_id}/${today.getFullYear()}-${today.getMonth()+1}-${today.getDate()}`)
+            let new_evo_ref = firebase.database().ref(`/environment_condition/${each_id}/${today.getFullYear()}-${today.getMonth()+1}-${today.getDate()}`)
             let push_fun = () => {
               console.log(`dht = ${dht_tot}`)
               if(dht_tot + 40 >= 70){
