21e9e66220e4b8db10712bc1418dd6c1b945323f
syan6010 <monday6010@gmail.com>
2019-10-23 13:51:19 +0800

    final01

--- index.js
+++ index.js
@@ -35,8 +35,8 @@ var large_a = ["薄荷", "迷迭香"]; //香草植物
 var large_b = ["橡皮樹", "發財樹"]; //大型室内植物
 
 
-let line_id_ref = firebase.database().ref(`user_device`)
-let plant_ref = firebase.database().ref(`plant_condition`)
+let line_id_ref = firebase.database().ref(`user_device`);
+let plant_ref = firebase.database().ref(`plant_condition`);
 
 
 
@@ -49,9 +49,9 @@ const today = new Date();
 
 bot.on('message', function (event) {
   if (event.message.type === 'text') {
-    let lineId = event.source.userId
-    let ref = firebase.database().ref(`user_device/${lineId}/step`)
-    let ref_pt = firebase.database().ref(`user_device/${lineId}/plantType`)
+    let lineId = event.source.userId;
+    let ref = firebase.database().ref(`user_device/${lineId}/step`);
+    let ref_pt = firebase.database().ref(`user_device/${lineId}/plantType`);
     let msg = event.message.text
 
 
@@ -139,24 +139,24 @@ bot.on('message', function (event) {
               break;
             case 1:
               if(mini.includes(msg)) { 
-                updateData(lineId, "dhtStandard", 75)
+                updateData(lineId, "dhtStandard", 75);
               } 
               else if(mini_a.includes(msg)){
-                updateData(lineId, "dhtStandard", 70)
+                updateData(lineId, "dhtStandard", 70);
               }
               else if(large.includes(msg)){
-                updateData(lineId, "dhtStandard", 60)
+                updateData(lineId, "dhtStandard", 60);
               }
               else if(large_a.includes(msg)){
-                updateData(lineId, "dhtStandard", 40)
+                updateData(lineId, "dhtStandard", 40);
               }
               else if(large_b.includes(msg)){
-                updateData(lineId, "dhtStandard", 50)
+                updateData(lineId, "dhtStandard", 50);
               }
               else {
-                updateData(lineId, "dhtStandard", 60)
+                updateData(lineId, "dhtStandard", 60);
               }
-              updateData(lineId, "plantType", msg)
+              updateData(lineId, "plantType", msg);
               event.reply({
                 type: 'template',
                 altText: '現在自動澆水功能都設定好嘍！！每週都會幫你推送我的健康數據，要好好照顧我喔！',
@@ -182,77 +182,43 @@ bot.on('message', function (event) {
                     .then(function(snapshot) {
                       let p_type = snapshot.val();
                       if(mini.includes(p_type)) { 
-                        event.reply('https://gcp.plant.lambda.tw/todo/?title=&category=1')
+                        event.reply('https://gcp.plant.lambda.tw/todo/?title=&category=1');
                       } 
                       else if(mini_a.includes(p_type)){
-                        event.reply('https://gcp.plant.lambda.tw/todo/?title=&category=5')
+                        event.reply('https://gcp.plant.lambda.tw/todo/?title=&category=5');
                       }
                       else if(large.includes(p_type)){
-                        event.reply('https://gcp.plant.lambda.tw/todo/?title=&category=2')
+                        event.reply('https://gcp.plant.lambda.tw/todo/?title=&category=2');
                       }
                       else if(large_a.includes(p_type)){
-                        event.reply('https://gcp.plant.lambda.tw/todo/?title=&category=1')
+                        event.reply('https://gcp.plant.lambda.tw/todo/?title=&category=1');
                       }
                       else if(large_b.includes(p_type)){
-                        event.reply('https://gcp.plant.lambda.tw/todo/?title=&category=3')
+                        event.reply('https://gcp.plant.lambda.tw/todo/?title=&category=3');
                       }
                       else {
-                        event.reply('https://gcp.plant.lambda.tw/todo/?title=&category=3')
+                        event.reply('https://gcp.plant.lambda.tw/todo/?title=&category=3');
                       }
-                      // switch(p_type) {
-                      //   case '虎尾蘭':
-                      //     event.reply('虎尾蘭');
-                      //     break;
-                      //   case '仙人掌':
-                      //     event.reply('仙人掌');
-                      //     break;
-                      //   case '黃金葛':
-                      //     event.reply('黃金葛');
-                      //     break;
-                      //   case '合果芋':
-                      //     event.reply('合果芋');
-                      //     break;
-                      //   case '茉莉花':
-                      //     event.reply('茉莉花');
-                      //     break;
-                      //   case '百合花':
-                      //     event.reply('百合花');
-                      //     break;
-                      //   case '薄荷':
-                      //     event.reply('薄荷');
-                      //     break;
-                      //   case '迷迭香':
-                      //     event.reply('迷迭香');
-                      //     break;
-                      //   case '橡皮樹':
-                      //     event.reply('橡皮樹');
-                      //     break;
-                      //   case '發財樹':
-                      //     event.reply('發財樹'); 
-                      //     break;
-                      //   default:
-                      //     event.reply('現在還沒有支援這種植物喔！抱歉');                       
-                      // }
                     })
                   break;
                 case '重設' :
                   step = -1
-                  event.reply('你好!!歡迎來到plantRobot!!第一次設定需要輸入webduino裝置的ID才可以讓我順利上網歐！!！')
+                  event.reply('你好!!歡迎來到plantRobot!!第一次設定需要輸入webduino裝置的ID才可以讓我順利上網歐！!！');
                   break;
                 case 'today' :
-                  event.reply(`${today.getFullYear()}-${today.getMonth()+1}-${today.getDate()}`)
+                  event.reply(`${today.getFullYear()}-${today.getMonth()+1}-${today.getDate()}`);
                   break;
                 default :
-                  event.reply('i cant do this!!')
+                  event.reply('詳細操作部分請參見使用手冊!!https://gcp.plant.lambda.tw/#');
               }
           }
           if (step > 1) { updateData(lineId, "step", 99) }
           else { updateData(lineId, "step", step + 1) }
         }
         else {
-            console.log('init')
-            initData(lineId)
-            event.reply('你好!!歡迎來到plantRobot!!第一次設定需要輸入webduino裝置的ID才可以讓我順利上網歐！！')
+            console.log('init');
+            initData(lineId);
+            event.reply('你好!!歡迎來到plantRobot!!第一次設定需要輸入webduino裝置的ID才可以讓我順利上網歐！！');
         }
       });
   }
@@ -287,7 +253,7 @@ let initData = (lineId) => {
 
 
 const  scheduleCronstyle = ()=>{
-    schedule.scheduleJob('30 26 * * * *', ()=>{
+    schedule.scheduleJob('30 53 * * * *', ()=>{
       plant_ref.once('value')
         .then(function(snapshot){
           snapshot.forEach(async function (childSnapshot){
@@ -295,29 +261,29 @@ const  scheduleCronstyle = ()=>{
             let temperature_tot = 0;
             let humidity_tot = 0;
             let each_id = childSnapshot.key
-            let new_plant_ref = firebase.database().ref(`/plant_condition/${each_id}/${today.getFullYear()}-${today.getMonth()+1}-${today.getDate()}`)
-            let new_evo_ref = firebase.database().ref(`/environment_condition/${each_id}/${today.getFullYear()}-${today.getMonth()+1}-${today.getDate()}`)
+            let new_plant_ref = firebase.database().ref(`/plant_condition/${each_id}/${today.getFullYear()}-${today.getMonth()+1}-${today.getDate()}`);
+            let new_evo_ref = firebase.database().ref(`/environment_condition/${each_id}/${today.getFullYear()}-${today.getMonth()+1}-${today.getDate()}`);
             let push_fun = () => {
               console.log(`dht = ${dht_tot}`)
               if(dht_tot + 40 >= 70){
-                bot.push(each_id, `狀況極佳！請繼續保持喔！今天我的總體溫濕度指標為${dht_tot/4 + 40}分，符合標準`)
+                bot.push(each_id, `狀況極佳！請繼續保持喔！今天我的總體溫濕度指標為${dht_tot/4 + 40}分，符合標準`);
               }
               else if(dht_tot/4 + 40 > 40 && dht_tot/4 + 40 < 70){
-                bot.push(each_id, `狀況普通！可以上我們的網站獲取植物冷知識，讓我變的更健康！今天我的總體溫濕度指標為${dht_tot/4 + 40}分，符合標準`)
+                bot.push(each_id, `狀況普通！可以上我們的網站獲取植物冷知識，讓我變的更健康！今天我的總體溫濕度指標為${dht_tot/4 + 40}分，符合標準`);
               } 
               else {
-                bot.push(each_id, `狀況不太好欸！可以上我們的網站獲取植物冷知識，加油吧！今天我的總體溫濕度指標為${dht_tot/4 + 40}分，不符合標準`)
+                bot.push(each_id, `狀況不太好欸！可以上我們的網站獲取植物冷知識，加油吧！今天我的總體溫濕度指標為${dht_tot/4 + 40}分，不符合標準`);
               }
             }
             new_plant_ref.once('value')
               .then(async function(snapshot) {
                   snapshot.forEach(function (childSnapshot) {
-                  let dht = childSnapshot.child("dht").val()
-                  dht_tot += dht 
-                  console.log(dht_tot)
-                  console.log('2')
+                  let dht = childSnapshot.child("dht").val();
+                  dht_tot += dht ;
+                  console.log(dht_tot);
+                  console.log('2');
                   })   
-                  await push_fun()  
+                  await push_fun();
 
               })
 
@@ -363,32 +329,32 @@ line_id_ref.once('value')
           let p_type = snapshot.val()
           if(mini.includes(p_type)) { 
             schedule.scheduleJob('30 26 * * * *',()=>{
-              bot.push(each_id, '多肉植物記得澆水嘍')           
+              bot.push(each_id, '多肉植物記得澆水嘍');        
             })      
           } 
           else if(mini_a.includes(p_type)){
             schedule.scheduleJob('30 19 * * * *',()=>{
-              bot.push(each_id, '芋科室內植物記得澆水嘍')       
+              bot.push(each_id, '芋科室內植物記得澆水嘍');      
             }) 
           }
           else if(large.includes(p_type)){
             schedule.scheduleJob('30 19 * * * *',()=>{
-              bot.push(each_id, '香花植物記得澆水嘍')         
+              bot.push(each_id, '香花植物記得澆水嘍');        
             }) 
           }
           else if(large_a.includes(p_type)){
             schedule.scheduleJob('30 19 * * * *',()=>{
-              bot.push(each_id, '香草植物記得澆水嘍')         
+              bot.push(each_id, '香草植物記得澆水嘍');     
             }) 
           }
           else if(large_b.includes(p_type)){
             schedule.scheduleJob('30 19 * * * *',()=>{
-              bot.push(each_id, '大型室内植物記得澆水嘍')         
+              bot.push(each_id, '大型室内植物記得澆水嘍');         
             }) 
           }
           else {
             schedule.scheduleJob('30 15 23 * * *',()=>{
-              bot.push(each_id, '????')         
+              bot.push(each_id, '????');         
             }) 
           }
         })
